3cfc0a6467b8dfbf6a5885543370a4b0672944aa
SanovaBeach <incredible.commanders@gmail.com>
2021-07-28 22:21:17 +0800

    added package

---
+++ src/data.js
@@ -0,0 +1,145 @@
+export const data = [
+  {
+    title: "Houses",
+    subMenu: [
+      {
+        title: "Brand1",
+        BrandMenu: [
+          {
+            title: "Name1",
+          },
+          {
+            title: "Name2",
+          },
+          {
+            title: "Name3",
+          },
+          {
+            title: "Name4",
+          },
+          {
+            title: "Name5",
+          },
+          {
+            title: "Name6",
+          },
+        ],
+      },
+      {
+        title: "Brand2",
+        BrandMenu: [
+          {
+            title: "Name1",
+          },
+          {
+            title: "Name2",
+          },
+          {
+            title: "Name3",
+          },
+          {
+            title: "Name4",
+          },
+          {
+            title: "Name5",
+          },
+          {
+            title: "Name6",
+          },
+        ],
+      },
+      {
+        title: "Brand3",
+        BrandMenu: [
+          {
+            title: "Name1",
+          },
+          {
+            title: "Name2",
+          },
+          {
+            title: "Name3",
+          },
+          {
+            title: "Name4",
+          },
+          {
+            title: "Name5",
+          },
+          {
+            title: "Name6",
+          },
+        ],
+      },
+      {
+        title: "Brand4",
+        BrandMenu: [
+          {
+            title: "Name1",
+          },
+          {
+            title: "Name2",
+          },
+          {
+            title: "Name3",
+          },
+          {
+            title: "Name4",
+          },
+          {
+            title: "Name5",
+          },
+          {
+            title: "Name6",
+          },
+        ],
+      },
+      {
+        title: "Brand5",
+        BrandMenu: [
+          {
+            title: "Name1",
+          },
+          {
+            title: "Name2",
+          },
+          {
+            title: "Name3",
+          },
+          {
+            title: "Name4",
+          },
+          {
+            title: "Name5",
+          },
+          {
+            title: "Name6",
+          },
+        ],
+      },
+      {
+        title: "Brand6",
+        BrandMenu: [
+          {
+            title: "Name1",
+          },
+          {
+            title: "Name2",
+          },
+          {
+            title: "Name3",
+          },
+          {
+            title: "Name4",
+          },
+          {
+            title: "Name5",
+          },
+          {
+            title: "Name6",
+          },
+        ],
+      },
+    ],
+  },
+];
